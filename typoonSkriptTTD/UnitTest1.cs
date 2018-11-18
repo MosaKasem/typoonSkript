61f62765fc61e427ce7c7e5e16cd2391260ce73c
MosaKasem <thescorpion119@gmail.com>
2018-11-18 19:02:51 +0100

    RED, DIVIDE FAILS 0

--- typoonSkriptTTD/UnitTest1.cs
+++ typoonSkriptTTD/UnitTest1.cs
@@ -21,46 +21,15 @@ namespace typoonSkriptTTD
             // assert
             Assert.Equal(expected, actual);
         }
-
-/*         [Fact]
-        public void CalculatorAdd()
+        
+        [Theory]
+        [InlineData(-5.0, 0, 0)]
+        public void Values_Should_Calculate_Divide(double x, double y, double expected)
         {
-
-            double expected = 5;
             var sut = new Calculator();
-            double actual = sut.Add(2, 3);
-            
+            var actual = sut.Divide(x, y);
+
             Assert.Equal(expected, actual);
         }
-        [Fact]
-        public void CalculatorSubtract()
-        {
-
-            double expected = 5;
-            var sut = new Calculator();
-            double actual = sut.Subtract(10, 5);
-            
-            Assert.Equal(expected, actual);
-        }
-        [Fact]
-        public void CalculatorMultiply()
-        {
-
-            double expected = 20;
-            var sut = new Calculator();
-            double actual = sut.Multiply(10, 2);
-            
-            Assert.Equal(expected, actual);
-        }
-        [Fact]
-        public void CalculatorDivide()
-        {
-
-            double expected = 2.5;
-            var sut = new Calculator();
-            double actual = sut.Divide(5.0, 2.0);
-            
-            Assert.Equal(expected, actual);
-        } */
     }
 }
