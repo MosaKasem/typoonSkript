b60573d8c299fe6c680584ee1847b0c8f796c6d4
MosaKasem <thescorpion119@gmail.com>
2018-11-27 22:36:20 +0100

    REFACTOR - MOVING STUFF AROUND TO mAKE MORE DEPENDENCIES BECAUSE ****

--- typoonSkriptTTD/UnitTest2.cs
+++ typoonSkriptTTD/UnitTest2.cs
@@ -13,6 +13,14 @@ namespace typoonSkriptTTD
             var sut = new Calculator(viewMock.Object);
             Assert.Throws<ArgumentException>(() => sut.IsEligable(Operation.Null));
         }
+        [Fact]
+        public void Test_App_With()
+        {
+            // app.Verify(m => m.application(), Times.AtLeastOnce());
+            var viewMock = new Mock<CalculatorView>();
+            var sut = new Calculator(viewMock.Object);
+            viewMock.Verify(s => s.ReturnValue("T"), Times.Once());
+        }
 
     }
 }
